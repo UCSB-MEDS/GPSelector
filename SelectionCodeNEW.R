f014e0b9b5e3a8aca7bde4bfefcb4b305ade7fa0
samanthacsik <samanthacsik@gmail.com>
2021-11-29 14:09:12 -0800

    testing MEDS 2021-22 data

--- SelectionCodeNEW.R
+++ SelectionCodeNEW.R
@@ -17,8 +17,12 @@ library(tidyverse)
 library(lpSolve)
 
 #..........................read in data..........................
-points <- read_csv(here::here("GP2017jc.csv")) # should be N x k matrix where N is number of students and k is num of projects
-proj_maximums <- read_csv(here::here("project_maximums.csv")) # should be k x 1 matrix
+# points <- read_csv(here::here("MEDS_2021_22", "MEDS_capstone_2022.csv")) # should be N x k matrix where N is number of students and k is num of projects
+# proj_maximums <- read_csv(here::here("MEDS_2021_22", "project_maximums.csv")) # should be k x 1 matrix
+
+# FAKE DATA
+points <- read_csv(here::here("test2021", "made_up", "GP2021_test9.csv"))
+proj_maximums <- read_csv(here::here("test2021", "made_up", "GP2021_test9_projMax.csv"))
 
 #..............drop names, convert points to matrix..............
 fmat <- points %>% 
@@ -31,7 +35,7 @@ B <- proj_maximums %>%
   as.matrix()
 
 #..........get number of students and project proposals..........
-k <- dim(B)[1] # projects
+k <- dim(B)[1] # project proposals
 N <-  dim(fmat)[1] # students
 
 #....................check that matrices match...................
@@ -45,9 +49,9 @@ f <- t(fmat) %>%
   gather(Project, Points) %>% 
   select(Points)
 
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##  ~ updated version of the above code (but values begin to differ? don't use this for now)  ----
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##  ~ updated version of the above code using pivot_* (but values begin to differ? don't use this for now)  ----
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # f <- t(fmat) %>% 
 #   as.data.frame() %>% 
 #   pivot_longer(cols = everything(), names_to = "Project", values_to = "Points") %>% # was gather()
@@ -55,7 +59,7 @@ f <- t(fmat) %>%
 #   select(Points)
 
 #........construct a diagonal matrix with dimensions of k........
-A0 <- diag(1, k) # k = 22, in the case of the 2017 class
+A0 <- diag(1, k) # k = 22, in the case of the 2017 class or 9 in the case of MEDS 2021-2022
 A <- A0
 
 #........use cbind on A and A0 to turn back into a matrix?........
@@ -64,7 +68,7 @@ for (i in 1:(N-1)){ # from 1 to the length of students - 1 (84, in the case of t
 }
 
 #..........create matrix of 0s with dimensions N x N*k..........
-# these are the student points? (from JC discussion)
+# these are the student points?? (from JC discussion, but not sure I see that)
 Aeq <- matrix(data = 0, nrow = N, ncol =  N*k)
 
 #................not sure what's going on here tbh...............
